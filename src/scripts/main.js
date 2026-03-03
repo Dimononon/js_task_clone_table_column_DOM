330f1bd2dc5c572c516f57f012a6e0e6efd2e07c
Dimononon <dimononk@gmail.com>
2026-03-03 15:56:31 +0200

    task complete

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+const newTh = document.createElement('th');
+
+table.tHead.rows[0].cells[3].after(newTh);
+
+newTh.innerHTML = table.tHead.rows[0].cells[1].innerHTML;
+
+const tbody = table.querySelector('tbody');
+const trs = tbody.querySelectorAll('tr');
+
+trs.forEach((tr) => {
+  const newTd = document.createElement('td');
+
+  tr.cells[3].after(newTd);
+  newTd.innerHTML = tr.cells[1].innerHTML;
+});
+
+const tfoot = table.querySelector('tfoot');
+const newTfootTh = document.createElement('th');
+
+tfoot.rows[0].cells[3].after(newTfootTh);
+
+newTfootTh.innerHTML = table.tHead.rows[0].cells[1].innerHTML;
